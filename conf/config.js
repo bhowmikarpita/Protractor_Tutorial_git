5a10f9b30ba0a223519151e787281bcb596364d5
bhowmikarpita <bhowmikarpita1222@gmail.com>
2024-04-18 00:38:46 +0530

    test

--- conf/config.js
+++ conf/config.js
@@ -18,7 +18,7 @@ exports.config = {
   // Framework to use. Jasmine is recommended.
   framework: 'jasmine',
 
-  // Spec patterns are relative to the current working directory when
+  // Spec patterns are relative to the current working directory when 
   // protractor is called.
   specs: ['../tests/calculator.js'],
 
